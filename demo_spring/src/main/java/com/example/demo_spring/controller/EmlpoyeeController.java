4fab8808ecdc4adafadf3a03b85a22c0e2c4a3f0
ROSSI2-PC\g.rossi <g.rossi@iemmeconsulting.it>
2023-04-18 16:59:02 +0200

    first commit

---
+++ demo_spring/src/main/java/com/example/demo_spring/controller/EmlpoyeeController.java
@@ -0,0 +1,60 @@
+package com.example.demo_spring.controller;
+
+
+import com.example.demo_spring.exception.ResourceNotFoundException;
+import com.example.demo_spring.model.Employee;
+import com.example.demo_spring.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("api/v1/employees")
+public class EmlpoyeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepo;
+
+    @GetMapping //return the list of all the employees
+    public List<Employee> getAllEmployees(){
+        return employeeRepo.findAll();
+    }
+
+    @PostMapping //create new employee
+    public Employee createEmployee(@RequestBody Employee employee){
+
+        return employeeRepo.save(employee);
+    }
+
+    @GetMapping("{id}")//get employee by id
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee = employeeRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("The employee with the id: " + id + " does not exist"));
+
+        return ResponseEntity.ok(employee);
+    }
+
+    @PutMapping("{id}")// update employee data
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails){
+        Employee updateEmployee = employeeRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("The employee with the id: " + id + " does not exist"));
+        updateEmployee.setFirstName(employeeDetails.getFirstName());
+        updateEmployee.setLastName(employeeDetails.getLastName());
+        updateEmployee.setEmailId(employeeDetails.getEmailId());
+
+        employeeRepo.save(updateEmployee);
+
+        return ResponseEntity.ok(updateEmployee);
+
+    }
+
+    @DeleteMapping("{id}")// delete employee
+    public ResponseEntity<Employee> deleteEmployee(@PathVariable Long id){
+        Employee employee =employeeRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("The employee with the id: " + id + "does not exist"));
+        employeeRepo.delete(employee);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
---
+++ demo_spring/src/main/java/com/example/demo_spring/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo_spring.repository;
+
+import com.example.demo_spring.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
